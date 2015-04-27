20ae9700b69d9cba8ccfb78fdcb5a3815c299668
krembo <kremboman2@yahoo.com>
2015-04-27 17:37:11 -0400

    changed to be jquery plugin

---
+++ jquery.hoverwave.js
@@ -0,0 +1,31 @@
+(function( $ ) {
+	$.fn.hoverWave = function (hoverColor) {
+		var $element = this,
+		letters = $element.text().split(''),
+		lettersHtml = '',
+		baseColor = $element.css('color'),
+		hoverColor = hoverColor || 'green';
+		for (var i in letters) {
+			lettersHtml += '<span class="ef-letter">' + letters[i] + '</span>';
+		}
+		$element.html(lettersHtml);
+
+		$element.mouseover(function () {
+			$(this).find('.ef-letter').each(function (index) {
+				$(this).css({
+					transition : 'color ' + (index * 0.05) + 's',
+					color : hoverColor
+				})
+			})
+		});
+		$element.mouseleave(function () {
+			$(this).find('.ef-letter').each(function (index) {
+				$(this).css({
+					transition : 'color ' + (index * 0.05) + 's',
+					color : baseColor
+				})
+			})
+		});
+		return this;
+	};
+}( jQuery ));
